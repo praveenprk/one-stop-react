84059f1ed98c94a9ecc8a1b1eb4b965b9ca104dc
praveenprk <praveenkumarkp666@gmail.com>
2023-03-21 01:25:40 +0530

    add task and time-based task

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../../App.css"
+import ShowToDo from './ShowToDo';
+
+
 const Todo = () => {
+
+  const [task, setTask] = useState(''); // for form only
+  const [allTasks, setAllTasks] = useState([]); // for list creation
+  const [isTimeSpecific, setIsTimeSpecific] = useState(false);
+  const [atTime, setAtTime] = useState('00:00');
+
+  useEffect(() => {
+    if(allTasks.length > 0)
+      localStorage.setItem("todo", JSON.stringify(allTasks));
+  }, [allTasks]);
+
+
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const updateTask = [
+      ...allTasks,
+      {
+        taskName: task,
+        date: new Date().toLocaleDateString(),
+        type: "work",
+        timeBound: isTimeSpecific ? true : false,
+        atTime: isTimeSpecific ? atTime : null,
+        completed: false
+      }
+    ];
+    setAllTasks(updateTask);
+  }
+
   return (
-    <div>Todo</div>
-  )
+    <>
+      <div className='d-flex flex-column gap-3'>
+        <form onSubmit={(e) => handleAdd(e)} className='d-flex flex-column gap-3'>
+        <h4>What needs to be done?</h4>
+        <div className='d-flex flex-row'>
+        <input
+        className="form-control"
+        type="text"
+        value={task}
+        onChange={(e) =>
+        {
+          e.preventDefault();
+          setTask(e.target.value)}
+        }
+        />&nbsp;
+        {/* For time specific todo */}
+        <span
+          onClick={() => setIsTimeSpecific(true)}
+          style={{
+            display: isTimeSpecific ? "none" : "block"
+          }}
+        >
+          ⏰
+        </span>
+        {
+          isTimeSpecific ?
+          <div className='d-flex flex-row'>
+            <input onChange={(e) => {
+              e.preventDefault();
+              setAtTime(e.target.value);
+            }} type="time"/>
+            <span onClick={() => setIsTimeSpecific(false)}>❌</span>
+          </div>
+          : ''
+        }
+        </div>
+        <input className='btn btn-warning' type="submit" value="Add" />
+        </form>
+        <ShowToDo getAllTasks={allTasks} />
+      </div>
+    </>
+  );
 }
 
 export default Todo
--- src/partials/header/Header.jsx
+++ src/partials/header/Header.jsx
@@ -8,28 +8,28 @@ const Header = () => {
   return (
     <Router>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
     <Link className='nav-link' to="/">Daily Digitals!</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
             <Link className="nav-link" to='/todo'>Todo</Link>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
             <Link className="nav-link" to='/events'>Events</Link>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             More
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Support</a></li>
-            {/* <li><a class="dropdown-item" href="#">Another action</a></li> */}
-            {/* <li><a class="dropdown-item" href="#">Something else here</a></li> */}
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Support</a></li>
+            {/* <li><a className="dropdown-item" href="#">Another action</a></li> */}
+            {/* <li><a className="dropdown-item" href="#">Something else here</a></li> */}
           </ul>
         </li>
       </ul>
