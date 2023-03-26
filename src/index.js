a8625f5583edfa1432e8d6e2b23a0fb7d1c87264
praveenprk <praveenkumarkp666@gmail.com>
2023-03-27 03:14:53 +0530

    removing reportwebvitals

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
@@ -17,8 +16,3 @@ root.render(
         </GoogleOAuthProvider>
     </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
