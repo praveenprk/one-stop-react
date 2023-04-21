8f732ddfbde53242f8e35f3bc7b227bcef594381
praveenprk <praveenkumarkp666@gmail.com>
2023-04-22 01:44:10 +0530

    got the calendar api working

--- src/googleapis/calendars.js
+++ src/googleapis/calendars.js
@@ -2,18 +2,34 @@ import axios from "axios";
 import { getUserEmail } from "../localDB/helpers";
 
 export const listAllCalendars = async () => {
-    const res = await axios.get(`https://www.googleapis.com/calendar/v3/calendars/${getUserEmail()}/events`, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      "Cross-Origin-Opener-Policy" : "Cross-Origin-Opener-Policy"
+    axios.get(`https://www.googleapis.com/calendar/v3/users/me/calendarList/${getUserEmail()}`,
+    {
+     headers: {
+      Authorization: `Bearer ${localStorage.getItem("access_token")} `,
     }
   }).then(res => console.log(res.data)).catch(err => console.error(err))
 }
 
 export const listCalendarEvents = async (calendarId) => {
-    const events = await axios.get("https://www.googleapis.com/calendar/v3/users/me/calendarList", {
+    const events = await axios.get(`https://www.googleapis.com/calendar/v3/calendars/${getUserEmail()}/events`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         }
-      }).then(res => console.log(res.data)).catch(err => console.error(err))
+      }).then(res => console.log(`events`,res.data)).catch(err => console.error(`events err`,err))
 }
+
+
+
+/* fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+   console.log(error)
+  })   */
