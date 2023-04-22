3c86f74449d03a2cd0ea912afbf5233641da989f
praveenprk <praveenkumarkp666@gmail.com>
2023-04-23 04:31:43 +0530

    adding calendar events to the reducers and store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import  todoSlicer from '../features/todo/todoSlicer'
 import userSlicer from '../features/users/userSlicer';
+import calEventsSlicer from '../features/calEvents/calEventsSlicer';
 
 export const store = configureStore({
   reducer: {
     todo: todoSlicer,
     user: userSlicer,
+    calEvents: calEventsSlicer,
   },
 });
