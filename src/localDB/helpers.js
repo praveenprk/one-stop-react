2e80325ba34a6968187fed6e0b214cd7c09706d3
praveenprk <praveenkumarkp666@gmail.com>
2023-04-23 04:30:58 +0530

    generating random guid for ID

--- src/localDB/helpers.js
+++ src/localDB/helpers.js
@@ -1,10 +1,31 @@
 export const getUserEmail = () => {
-    let userInfo = localStorage.getItem("userInfo");
+    let userInfo = localStorage?.getItem("userInfo");
     userInfo = JSON.parse(userInfo);
-    return userInfo.email;
+    return userInfo?.email;
+}
+
+export const getAccessToken = () => {
+    return localStorage?.getItem("access_token");
 }
 
 export const saveAccessTokenToLS = (access_token) => {
-    localStorage.setItem("access_token", access_token);
-    if(localStorage.getItem("access_token")) return true
+    localStorage?.setItem("access_token", access_token);
+    if(localStorage?.getItem("access_token")) return true
+}
+
+export const parseDateFromISO = (ISO_date_string) => {
+    return Date.parse(ISO_date_string);
+}
+
+export const compareDates = (datetimestring) => {
+   let parsedDate = parseDateFromISO(datetimestring)
+
+//    parsedDate <
+}
+
+export const guidGenerator = () => {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 }
