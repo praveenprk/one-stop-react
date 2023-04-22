9cdc75b89801ca3a6f76f77d273b0fa04a0d212e
praveenprk <praveenkumarkp666@gmail.com>
2023-04-23 04:33:47 +0530

    returning promise after calling Google API to be consumed by the Events Comp

--- src/googleapis/calendars.js
+++ src/googleapis/calendars.js
@@ -1,35 +1,22 @@
 import axios from "axios";
-import { getUserEmail } from "../localDB/helpers";
+import { getUserEmail, getAccessToken } from "../localDB/helpers";
 
-export const listAllCalendars = async () => {
-    axios.get(`https://www.googleapis.com/calendar/v3/users/me/calendarList/${getUserEmail()}`,
+let userEmail = getUserEmail();
+let access_token = getAccessToken();
+
+export const getAllCalendars = async () => {
+    axios.get(`https://www.googleapis.com/calendar/v3/users/me/calendarList/${userEmail}`,
     {
      headers: {
-      Authorization: `Bearer ${localStorage.getItem("access_token")} `,
+      Authorization: `Bearer ${access_token}`,
     }
-  }).then(res => console.log(res.data)).catch(err => console.error(err))
+  });
 }
 
-export const listCalendarEvents = async (calendarId) => {
-    const events = await axios.get(`https://www.googleapis.com/calendar/v3/calendars/${getUserEmail()}/events`, {
+export const getCalendarEvents = (calendarId) => {
+   return axios.get(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          Authorization: `Bearer ${access_token}`,
         }
-      }).then(res => console.log(`events`,res.data)).catch(err => console.error(`events err`,err))
+      });
 }
-
-
-
-/* fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-  })
-  .catch(error => {
-   console.log(error)
-  })   */
