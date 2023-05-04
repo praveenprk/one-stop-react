a146b3906a4fdb7410fdb8d950185c7282b8563c
praveenprk <praveenkumarkp666@gmail.com>
2023-05-05 01:51:50 +0530

    feat: delete event from redux & users calendar

--- src/components/events/AllEvents.jsx
+++ src/components/events/AllEvents.jsx
@@ -3,6 +3,8 @@ import { store } from '../../store/store';
 import ShowAttendees from './ShowAttendees';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { deleteEventFromCalendarAPI } from '../../googleapis/calendars';
+import { deleteEvent } from '../../features/calEvents/calEventsSlicer';
 
 const AllEvents = () => {
 
@@ -10,6 +12,7 @@ const eventsInState = store.getState().calEvents;
 // const [basicModal, setBasicModal] = useState(false);
 const [modalShow, setModalShow] = useState(false);
 const [eventInfo, setEventInfo] = useState([]);
+const [deleted, setDeleted] = useState(false);
 
 function MyVerticallyCenteredModal(props) {
   const { eventInfo } = props
@@ -30,12 +33,12 @@ function MyVerticallyCenteredModal(props) {
       <hr></hr>
       <div className='d-flex flex-column gap-2'>
         {
-        eventInfo?.attendees?.map((names, i) =>
-          <div key={i} className='d-flex flex-row justify-content-around'>
-            <span className='badge bg-primary flex-wrap rounded-pill'>{`${names.email}`}</span>
-            <p>{`${names?.responseStatus}`}</p>
-          </div>
-          )
+        eventInfo?.attendees?.map((names, i) => 
+          <ul key={i} className='d-flex flex-row justify-content-around'>
+            <li class="badge bg-secondary">{`${names.email}`}</li>
+            <li class="badge bg-secondary">{`${names?.responseStatus}`}</li>
+          </ul>
+        )
           }
       </div>
       <hr></hr>
@@ -56,10 +59,18 @@ function MyVerticallyCenteredModal(props) {
       <div className='container d-flex flex-column'>
         <strong>
           {eventsInState?.map((val, i) => {
-            // val?.start?.dateTime
+            // console.log(`delete`, val);
             return (
               <div key={i} className="d-flex flex-column card-body shadow-lg p-3 mb-5 bg-white rounded">
-                <h5 className="card-title">{val?.summary}</h5>
+                <h5 className="card-title">{val?.summary}</h5><span onClick={
+                  () => { 
+                    store.dispatch(deleteEvent(val?.eventId));
+                    deleteEventFromCalendarAPI(val?.eventId)
+                   .then(() => alert(`Event ${val?.summary} deleted`))
+                   .catch(err => console.error(`error in deleting:`, err))
+                   .finally(() => setDeleted(true));             
+                  }
+                  }> Delete </span>
                 <hr></hr>
                 <small className="pb-2 card-text">Starts @ 🕗&nbsp;:{`${val?.start?.dateTime}`}</small>
                 <small className="p-2 card-text">Events status&nbsp;: {`${val?.eventStatus}`}</small>
